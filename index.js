c1b2f5189048d991ea2be4a93cbd32f1924c05ef
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-01-29 00:19:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Alex"
-var height = "6"
+var height = 6
 
 console.log("Name:")
 console.log(name)
 
-console.log(parseInt(height) + 1 )
+console.log(height + 1)
 console.log(height)
